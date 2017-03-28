72265a34aea653958e5c577b860b458205610a72
samuraiseoul <scottlavigne28@gmail.com>
2017-03-27 19:58:22 -0500

    commented and cleaned up code

--- _plugins/listing_tag.rb
+++ _plugins/listing_tag.rb
@@ -10,26 +10,41 @@ module Jekyll
     end
 
     def render(context)
-        output = ''
+        sections = getSections(context.registers[:site].posts)
+        "#{getOutput(sections, context.registers[:site].baseurl)}"
+    end
+
+    def getSections(posts)
         sections = Array.new
-        for post in context.registers[:site].posts
+        for post in posts
+            #compile all posts into arrays of posts organized by section
             section = post.data['listing']['section']['order'] - 1
             order = post.data['listing']['order'] - 1
-            if sections[section].nil?
+            if sections[section].nil? #create new array if that section hasn't been initialized
               sections[section] = Array.new
             end
             sections[section].push post
         end
-        sections.sort! {|a, b| a[0].data['listing']['section']['order'] <=> b[0].data['listing']['section']['order']}
-        for section in sections
+        #sort sections, there must be a section with an order of 1 or this will fail
+        sections.sort! {|a, b| a[0].data['listing']['section']['order'] <=> b[0].data['listing']['section']['order']} 
+        sections
+    end
+
+    def getOutput(sections, baseUrl)
+      output = ''
+      for section in sections
+          #Add section header for each section and sort posts in each section
           output += "<h2>#{section[0].data['listing']['section']['order']}. #{section[0].data['listing']['section']['title']}</h2>"
-        section.sort! {|a, b| a.data['listing']['order'] <=> b.data['listing']['order']}
+          section.sort! {|a, b| a.data['listing']['order'] <=> b.data['listing']['order']}
+
+          #ouput each post link
           for post in section
-            output += "<h3><a href='#{context.registers[:site].baseurl}#{post.url}'>#{post.data['listing']['order']}. #{post.data['title']}</a></h3>"
+            output += "<h3><a href='#{baseUrl}#{post.url}'>#{post.data['listing']['order']}. #{post.data['title']}</a></h3>"
           end
         end
-        "#{output}"
+        output
     end
+
   end
 end
 
