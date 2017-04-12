51a8f475bf6368e54fdbce6fe40b4bdf2ae955c2
samuraiseoul <scottlavigne28@gmail.com>
2017-04-12 00:02:26 -0500

    added publishing script

---
+++ publish.sh
@@ -0,0 +1,19 @@
+mv .gitignore .gitignore-local
+
+mv .gitignore-publish .gitignore
+
+bundle exec jekyll build
+
+git add -A
+
+git commit -m "publish"
+
+git subtree push --prefix _site/ origin gh-pages
+
+mv .gitignore .gitignore-publish
+
+mv .gitignore-local .gitignore
+
+git reset HEAD~1
+
+git stash
